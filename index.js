7b293e1b1489c9d71705d0c02d70c03e89b601c4
anirudh <anirudhece2020@gmail.com>
2022-05-26 17:07:50 +0530

    update

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 let pri,intr,time,calc ;
 pri= document.getElementById("principal").value;
+// pri= parseInt(pri);
+
 intr=document.getElementById("interest").value;
+// intr= parseInt(intr);
+
 time=document.getElementById("time").value;
+// time= parseInt(time);
 
 calc=document.getElementById("but");
 
-let cintr= pri* (Math.pow(1+ (intr/100) ) ) ;
-
-
+var cintr= pri * (Math.pow( (intr/100) + 1 ), time ) ;
 
 calc.addEventListener('click',(e)=>{  
     console.log(pri);
     console.log(intr);
     console.log(time);
-
     console.log(cintr);
-
+    
+    location.reload();
 })
